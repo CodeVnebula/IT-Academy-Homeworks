53c126b86c35e2615b6c5fa360ea2d248b9813cc
CodeVnebula <chkhikvadzem471@gmail.com>
2024-02-27 23:05:01 +0400

    day 2 finished

---
+++ homework_2/task_3.py
@@ -0,0 +1,26 @@
+while True:
+    number = int(input("Enter the number between 1 and 10: "))
+    if number <= 10 and number > 0:
+        break
+
+if number == 10:
+    print(2, 5, end="")
+elif number == 9: 
+    print(3)
+elif number == 8:
+    print(2)
+elif number == 7:
+    print(7)
+elif number == 6:
+    print(2, 3, end="")
+elif number == 5:
+    print(5)
+elif number == 4: 
+    print(2)
+elif number == 3:
+    print(3)
+elif number == 2:
+    print(2)
+
+
+
