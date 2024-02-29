24d6d36d8a9e8ef2f41334eb8e68e956af05e598
CodeVnebula <chkhikvadzem471@gmail.com>
2024-02-29 21:27:23 +0400

    Removed task files from homework_1 folder

--- homework_1/task_1.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ")
-second_name = input ("Enter your surname: ")
-age = int(input("Enter your age: "))
-print("Hello", name, second_name, "you are", age, "years old!")
