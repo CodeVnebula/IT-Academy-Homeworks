86eced929e982a77081e814ba98d888093a414e7
CodeVnebula <chkhikvadzem471@gmail.com>
2024-03-02 12:35:37 +0400

    task 4 in progress

---
+++ homework_3/task_1.py
@@ -0,0 +1,6 @@
+import math
+
+x = int(input("Enter X: "))
+y = int(input("Enter Y: "))
+
+print("X ^ Y = ", math.pow(x, y))
---
+++ homework_3/task_3.py
@@ -0,0 +1,41 @@
+import random
+
+symbol = random.randint(1, 4)
+
+if symbol == 1:
+    print("Clubs / ", end="", sep="")
+elif symbol == 2:
+    print("Diamonds / ", end="", sep="")
+elif symbol == 3:
+    print("Hearts / ", end="", sep="")
+else:
+    print("Spades / ", end="", sep="")
+
+card = random.randint(1, 13)
+
+if card == 1:
+    print("2", end="", sep="")
+elif card == 2:
+    print("3", end="", sep="")
+elif card == 3:
+    print("4", end="", sep="")
+elif card == 4:
+    print("5", end="", sep="")
+elif card == 5:
+    print("6", end="", sep="")
+elif card == 6:
+    print("7", end="", sep="")
+elif card == 7:
+    print("8", end="", sep="")
+elif card == 8:
+    print("9", end="", sep="")
+elif card == 9:
+    print("10", end="", sep="")
+elif card == 10:
+    print("Jack", end="", sep="")
+elif card == 11:
+    print("Queen", end="", sep="")
+elif card == 12:
+    print("King", end="", sep="")
+else:
+    print("Ace", end="", sep="")
