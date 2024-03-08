e3ee511ee00bd52f79fee133d0af75916bca1000
CodeVnebula <chkhikvadzem471@gmail.com>
2024-03-08 16:44:58 +0400

    new hom 2 br

---
+++ homework_2/task_2.py
@@ -0,0 +1,11 @@
+a = int(input("Enter A: "))
+b = int(input("Enter B: "))
+
+if b != 0:
+    if a % b == 0:
+        print("A is a multiple of B")
+    else:
+        print("A is not a multiple of B")
+else:
+    print("Can't divide by 0! Enter B again.")  
+    
---
+++ homework_2/task_3.py
@@ -0,0 +1,18 @@
+car_speed = int(input("Enter the velocity of the car: "))
+
+if car_speed > 0:
+    # if car_speed > 500:
+    #      print("What is bro even driving (skull emoji missing)?")
+    # elif car_speed == 300:
+    #      print('"Kerosene" started playing!')
+    if car_speed > 120:
+        print("Category: very fast")
+    elif car_speed > 60:
+        print("Category: fast")
+    elif car_speed >= 30:
+        print("Category: moderate")
+    else:
+        print("Category: slow")
+else:
+    print("Let's put the car in the first gear :)")
+    
