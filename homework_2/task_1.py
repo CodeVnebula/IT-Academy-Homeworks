6c0e03ce39b15c6e1dcb05f2df02d3ff6f48f480
Nexus <121356447+CodeVnebula@users.noreply.github.com>
2024-03-13 22:46:54 +0400

    Update task_1.py

--- homework_2/task_1.py
+++ homework_2/task_1.py
@@ -3,10 +3,6 @@ while True:
     if true_or_false == 'true' or true_or_false == 'false': # anything different from true or false
         break
 
-
-# if true_or_false != 'true' or true_or_false != 'false':
-#     print("Please enter only 'true' or 'false'!")
-
 if true_or_false == 'true':
     print("whoala!")  
 else:
