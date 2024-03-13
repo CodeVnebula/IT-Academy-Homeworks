dd76ed7498aa7a261bfa826575bbf1ce919868d3
Nexus <121356447+CodeVnebula@users.noreply.github.com>
2024-03-13 22:45:38 +0400

    Update task_3.py

--- homework_2/task_3.py
+++ homework_2/task_3.py
@@ -1,15 +1,11 @@
 car_speed = int(input("Enter the velocity of the car: "))
 
 if car_speed > 0:
-    # if car_speed > 500:
-    #      print("What is bro even driving (skull emoji missing)?")
-    # elif car_speed == 300:
-    #      print('"Kerosene" started playing!')
     if car_speed > 120:
         print("Category: very fast")
     elif car_speed > 60:
         print("Category: fast")
-    elif car_speed >= 30:
+    elif car_speed > 30:
         print("Category: moderate")
     else:
         print("Category: slow")
