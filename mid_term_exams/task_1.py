e9e576d02038664c4b6d71fc88a40dc637213027
CodeVnebula <chkhikvadzem471@gmail.com>
2024-04-15 20:47:23 +0400

    Updated task

--- mid_term_exams/task_1.py
+++ mid_term_exams/task_1.py
@@ -1,7 +1,14 @@
-name = input("Enter your name: ")
-surname = input("Enter your surname: ")
-age = int(input("Enter your age: "))
+def main():
+    name = input("Enter your name: ")
+    surname = input("Enter your surname: ")
+    age = int(input("Enter your age: "))
 
-print(f"Hello {name} {surname}.")
-print(f"Age: {age}")
+    if age <= 0:
+        print("You have not been born yet!")    
+        return
 
+    print(f"Hello {name} {surname}.")
+    print(f"Age: {age}")
+
+if __name__ == "__main__":
+    main()
