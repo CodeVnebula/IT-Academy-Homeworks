1fed0e77445fbf7419ffa467a574a83c80765037
CodeVnebula <chkhikvadzem471@gmail.com>
2024-04-27 21:56:25 +0400

    Added task_1

---
+++ homework_21/task_2.py
@@ -0,0 +1,15 @@
+graph = {
+    "A" : ["B", "C"],
+    "B" : ["D"],
+    "C" : ["E"]
+}
+
+def check_path(_from, to) -> bool:
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
