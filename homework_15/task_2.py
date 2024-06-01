fbea3cefc6666bef9c909f161c5a887ecae32536
CodeVnebula <chkhikvadzem471@gmail.com>
2024-06-01 19:46:44 +0400

    Updated as suggested

--- homework_15/task_2.py
+++ homework_15/task_2.py
@@ -1,5 +1,5 @@
 def is_prime(number):
-    print("Num -", number, " " ,end="")
+    print(f"Num - {number} ", end="")
     if number <= 1:
         return False
     for i in range(2, int(number**0.5) + 1):
